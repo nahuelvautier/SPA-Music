a5bb0b11880cb6b37768950558de1f226de1b998
Mr. Robot <nahuelearning@gmail.com>
2021-08-01 23:16:49 -0300

    Link Change Test

--- app/components/MusicVideosPost.js
+++ app/components/MusicVideosPost.js
@@ -23,7 +23,7 @@ export function MusicVideos (props) {
 
   $iframe.width = "640";
   $iframe.height = "360";
-  $iframe.src = ENDPOINT_EMBED;
+  $iframe.src = `${ENDPOINT_EMBED};origin=https://nahuelvautier.github.io/`;
   $iframe.title = "YouTube video player";
   $iframe.frameborder = "1";
   $iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
